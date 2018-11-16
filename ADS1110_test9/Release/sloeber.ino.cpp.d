38c6089f768b6721b8cf8d21ebba43d9fde61d22
Szymon <Szymon@192.168.1.100>
2018-11-16 22:38:11 +0100

    noew file

---
+++ ADS1110_test9/Release/sloeber.ino.cpp.d
@@ -0,0 +1,106 @@
+sloeber.ino.cpp.o: ..\sloeber.ino.cpp \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/stdlib_noniso.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/binary.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/esp8266_peri.h \
+ E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/twi.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/core_esp8266_features.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h \
+ E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/ets_sys.h \
+ E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h \
+ E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/eagle_soc.h \
+ E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/osapi.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/user_config.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WCharacter.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WString.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/HardwareSerial.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Stream.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Print.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Printable.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/uart.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Esp.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Updater.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/flash_utils.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/MD5Builder.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/md5.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/debug.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\variants\nodemcu/pins_arduino.h \
+ E:\ESP8266\sloeber\arduinoPlugin\libraries\ADS1110\1.3.0/ADS1110.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\libraries\Wire/Wire.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Stream.h \
+ E:\ESP8266\sloeber\arduinoPlugin\libraries\ADS1110\1.3.0/utility/ADS1110_PString.h \
+ E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Print.h \
+ ..\ADS1110_Usage.ino
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/stdlib_noniso.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/binary.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/esp8266_peri.h:
+
+E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/twi.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/core_esp8266_features.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h:
+
+E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/ets_sys.h:
+
+E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h:
+
+E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/eagle_soc.h:
+
+E:\ESP8266\sloeber\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/osapi.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/user_config.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WCharacter.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WString.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/HardwareSerial.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Stream.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Print.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Printable.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/uart.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Esp.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Updater.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/flash_utils.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/MD5Builder.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/md5.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/debug.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\variants\nodemcu/pins_arduino.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\libraries\ADS1110\1.3.0/ADS1110.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\libraries\Wire/Wire.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Stream.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\libraries\ADS1110\1.3.0/utility/ADS1110_PString.h:
+
+E:\ESP8266\sloeber\arduinoPlugin\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Print.h:
+
+..\ADS1110_Usage.ino:
---
+++ ADS1110_test9/sloeber.ino.cpp
@@ -0,0 +1,35 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-11-16 22:14:28
+
+#include "Arduino.h"
+#include <ADS1110.h>
+
+void setup() ;
+void runTests() ;
+void loop() ;
+void testPing() ;
+void testConMode() ;
+void testRes() ;
+void testSampleRate() ;
+void testVref() ;
+void testReset() ;
+void testGetSettings() ;
+void testGetConMode() ;
+void testGetRes() ;
+void testGetSampleRate() ;
+void testGetVref() ;
+void testGetData() ;
+void testGetVolt() ;
+void testGetPercent() ;
+void testGetComResult() ;
+void printDivider() ;
+void quickDelay() ;
+void longDelay() ;
+
+
+#include "ADS1110_Usage.ino"
+
+#endif
