95b4a06af3479c8706b84ce6f281bee18c1218e2
Szymon <Szymon@192.168.1.100>
2018-11-16 22:31:48 +0100

    First commit

---
+++ 1wire_dallas_test5/sloeber.ino.cpp
@@ -0,0 +1,19 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-09-03 22:03:09
+
+#include "Arduino.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+void setup(void) ;
+void printTemperature(DeviceAddress deviceAddress) ;
+void loop(void) ;
+void printAddress(DeviceAddress deviceAddress) ;
+
+
+#include "Tester.pde"
+
+#endif
